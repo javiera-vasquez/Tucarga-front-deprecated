c63c3eac129ab93af874c83937f00c8e6144de5f
Javier Enrique <s0wkete@gmail.com>
2013-12-02 11:04:54 -0300

    Creando modelo y data

---
+++ app/scripts/models/models.js
@@ -0,0 +1,103 @@
+YoEmber.Transportista = DS.Model.extend({
+    name:   DS.attr(),
+    rut: DS.attr(),
+    label: DS.attr(),
+    userName: DS.attr(),
+    userMail: DS.attr(),
+    userCel: DS.attr(),
+    userPhone: DS.attr(),
+    address: DS.attr(),
+    commune: DS.attr(),
+    city: DS.attr()
+});
+
+YoEmber.Transportista.FIXTURES = [{
+    id: 1,
+    name: 'Super duper emprresa',
+    rut: '16.123.435-8',
+    label: '',
+    userName: 'Pedro piedra',
+    userMail: 'asdfasdf@asdf.com',
+    userPhone: '+56 2 2287 2309',
+    userCel: '+56 9 2345 9876',
+    address: 'Calle falsa #123',
+    commune: 'Las condes',
+    city: 'Santiago'
+},{
+    id: 2,
+    name: 'asdf duper emprresa',
+    rut: '16.123.435-8',
+    label: '',
+    userName: 'Pedro piedra',
+    userMail: 'asdfasdf@asdf.com',
+    userPhone: '+56 2 2287 2309',
+    userCel: '+56 9 2345 9876',
+    address: 'Calle falsa #123',
+    commune: 'Las condes',
+    city: 'Santiago'
+},{
+    id: 3,
+    name: 'Super asdfr emprresa',
+    rut: '16.123.435-8',
+    label: '',
+    userName: 'Pedro piedra',
+    userMail: 'asdfasdf@asdf.com',
+    userPhone: '+56 2 2287 2309',
+    userCel: '+56 9 2345 9876',
+    address: 'Calle falsa #123',
+    commune: 'Las condes',
+    city: 'Santiago'
+},{
+    id: 4,
+    name: 'Super duper eiuasdf',
+    rut: '16.123.435-8',
+    label: '',
+    userName: 'Pedro piedra',
+    userMail: 'asdfasdf@asdf.com',
+    userPhone: '+56 2 2287 2309',
+    userCel: '+56 9 2345 9876',
+    address: 'Calle falsa #123',
+    commune: 'Las condes',
+    city: 'Santiago'
+},{
+    id: 5,
+    name: 'qwerqwerr emprresa',
+    rut: '16.123.435-8',
+    label: '',
+    userName: 'Pedro piedra',
+    userMail: 'asdfasdf@asdf.com',
+    userPhone: '+56 2 2287 2309',
+    userCel: '+56 9 2345 9876',
+    address: 'Calle falsa #123',
+    commune: 'Las condes',
+    city: 'Santiago'
+},{
+    id: 6,
+    name: 'opiavm, uyidsafkm',
+    rut: '16.123.435-8',
+    label: '',
+    userName: 'Pedro piedra',
+    userMail: 'asdfasdf@asdf.com',
+    userPhone: '+56 2 2287 2309',
+    userCel: '+56 9 2345 9876',
+    address: 'Calle falsa #123',
+    commune: 'Las condes',
+    city: 'Santiago'
+}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
