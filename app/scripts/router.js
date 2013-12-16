d4156d555baab4c623f5d6556e247a9bc98d688f
Javier <Vasquez>
2013-12-16 12:25:51 -0300

    Create nuevas rutas

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,31 +1,21 @@
 YoEmber.Router.map(function () {
+    // Inicio
     this.resource('index', { path: '/' });
-    this.resource('cargador');
 
-    // Rutas de la vista transportista
-    this.resource('transportista', function(){
-        // Crea un usuario de tipo transportista
+    //Directorio transportistas
+    this.resource('directorio');
+
+    // Registro de usuario
+    this.resource('registro', function{
         this.route('create');
-        // Validados
-        this.resource('validados', function(){
-            this.resource('user', {path: '/:user_id'}, function(){
-                this.route('edit');
-            });
-        });
-        // Prospectos
-        this.resource('prospectos', function(){
-            this.resource('prospecto', {path: '/:user_id'}, function(){
-                this.route('edit');
-            });
-        });
-        // Rechazados
-        this.resource('rechazados', function(){
-            this.resource('rechazado', {path: '/:user_id'}, function(){
-                this.route('edit');
-            });
-        });
     });
 
+    // Nosotros
+    this.resource('nosotros', function(){
+        this.rosource('contacto'):
+        this.rosource('preguntas-frecuentes'):
+        this.rosource('como-funciona'):
+    });
 });
 
 
